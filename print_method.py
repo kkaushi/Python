7064ec982bff05729ec03b8b9239959b1c693565
kkaushi <2kuldeepkaushik@gmail.com>
2014-06-19 00:30:03 -0700

    First comit

---
+++ print_method.py
@@ -0,0 +1,4 @@
+print("hello Python!");
+print("I think this is \"FUN\"");
+print('I think this is \"Fun\" too');
+#This is a comment
---
+++ variables_operators_strings.py
@@ -0,0 +1,24 @@
+#variables and math
+cars = 100
+space_in_a_car = 4.0
+drivers = 30 
+passengers = 90
+cars_not_driven=cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("There are ",cars," cars available")
+print("We need to put about ", average_passengers_per_car," in each car.")
+
+#strings
+my_name = 'kuldeep kaushik'
+my_age = 27
+my_education = 'masters'
+
+print("Let's talk about %s." % my_name)
+print("He is %d years old and is doing his %s."%(my_age,my_education));
+
+two = 2
+three = 3
+print("%d plus %d is %d"%(two,two,two+three));
